cdddadadec2d0188d3a70139ad9d45fb7c393903
Chansik Yun <chansigi@gmail.com>
2018-11-21 15:22:39 +0900

    .

--- EmbeddedSystem/1121/fnd/led.c
+++ EmbeddedSystem/1121/fnd/led.c
@@ -22,31 +22,38 @@ void ledColorSet(uchar r_val, uchar g_val, uchar b_val)
 	softPwmWrite(LedPinBlue,  b_val);
 }
 
+void off() {
+	pwmWrite(LedPinRed,   0);
+	pwmWrite(LedPinGreen, 0);
+	softPwmWrite(LedPinBlue,  0);
+}
+
 int main(void)
 {
 	wiringPiSetup();
 	ledInit();
 
-	while(1){
-		ledColorSet(0xff,0x00,0x00);   //red
-		delay(500);
-		ledColorSet(0x00,0xff,0x00);   //green
-		delay(500);
-		ledColorSet(0x00,0x00,0xff);   //blue
-		delay(500);
-		ledColorSet(0xff,0xff,0x00);   //yellow
-		delay(500);
-		ledColorSet(0xff,0x00,0xff);   //pick
-		delay(500);
-		ledColorSet(0xc0,0xff,0x3e);
-		delay(500);
-		ledColorSet(0x94,0x00,0xd3);
-		delay(500);
-		ledColorSet(0x76,0xee,0x00);
-		delay(500);
-		ledColorSet(0x00,0xc5,0xcd);
-		delay(500);
-	}
+	ledColorSet(0xff,0x00,0x00);   // red
+	delay(500);
+	ledColorSet(0x00,0xff,0x00);   // green
+	delay(500);
+	ledColorSet(0x00,0x00,0xff);   // blue
+	delay(500);
+	ledColorSet(0xff,0xff,0x00);   // yellow
+	delay(500);
+	ledColorSet(0xff,0x00,0xff);   
+	delay(500);
+	ledColorSet(0xc0,0xff,0x3e);
+	delay(500);
+	ledColorSet(0x94,0x00,0xd3);
+	delay(500);
+	ledColorSet(0x76,0xee,0x00);
+	delay(500);
+	ledColorSet(0x00,0xc5,0xcd);
+	delay(500);
+
+	off();
+	getchar();
 	return 0;
 }
 
---
+++ EmbeddedSystem/1121/fnd/tone.c
@@ -0,0 +1,25 @@
+#include <wiringPi.h>
+#include <softTone.h>
+
+#include <stdio.h>
+
+#define PIN 21
+
+int scale [8] = { 262, 294, 330, 349, 392, 440, 494, 525 } ;
+int main ()
+{
+	int i ;
+	wiringPiSetup() ;
+	softToneCreate(PIN) ;
+
+	while (1)
+	{
+		for (i = 0 ; i < 8 ; ++i)
+		{
+			printf ("%3d\n", i);
+			softToneWrite (PIN, scale [i]);
+			delay (500);
+		}
+	}
+}
+
